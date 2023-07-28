b75eec6fb04946d0344b752cdce33cb2f3d5a8a5
sseongeun <cse0522@naver.com>
2023-07-29 01:36:35 +0900

    [MOD]코드 수정

--- trothcam_web/src/components/Register.jsx
+++ trothcam_web/src/components/Register.jsx
@@ -9,127 +9,177 @@ import { useNavigate } from 'react-router-dom';
 
 // 페이지의 크기를 지정하는 스타일드 컴포넌트를 만듭니다.
 const PageContainer = styled.div`
-    position:relative;
-    width: 1440px;
-    height: 1288px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+ 
     background-color: #ffffff;
-
+   
 `;
 
 const FooterContainer = styled.footer`
-    position: absolute;
-    width: 1440px;
-    height: 237px;
-    bottom: 0; // 'top' 대신 'bottom'을 사용하여 컨테이너의 하단에 위치하도록 합니다.
     display: flex;
+    justify-content: center;
+    margin-top:100px;
+  
+    height: 237px;
     align-items: center;
     background-color: #E8EAEF;
 `;
 
 
 const RegisterText = styled.h1`
-    position: absolute;
-    width: 295px;
-    height: 97px;
-    top: 174px;
-    left: 571px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+   
+
     font-family: Inter;
     font-size: 80px;
     font-weight: 400;
     line-height: 97px;
     letter-spacing: 0em;
-    text-align: left;
     color: #000000;
+
+    @media (max-width:1100px) {
+        font-size: 40px;
+        line-height: 48px;
+      }
 `;
 
 const RegisterText1 = styled.h1`
-    position: absolute;
-    width: 1059px;
-    height: 42px;
-    top: 338px;
-    left: 205px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  
+  
     font-family: Inter;
     font-size: 35px;
     font-weight: 400;
     line-height: 42px;
     letter-spacing: 0em;
-    text-align: center;
-
     color: #000000;
+
+    @media (max-width:1100px) {
+        font-size: 17px;
+        line-height: 21px;
+      }
+
 `;
 
 const RegisterText2 = styled.h1`
-    position: absolute;
-    width: 426px;
-    height: 34px;
-    top: 419px;
-    left: 507px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
     font-family: Inter;
     font-size: 28px;
     font-weight: 400;
     line-height: 34px;
     letter-spacing: 0em;
-    text-align: center;
-
-
     color: #000000;
+
+    @media (max-width:1100px) {
+        font-size: 14px;
+        line-height: 17px;
+      }
+
+
 `;
 
+const CheckboxContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top:35px;
+    
+`;
+
+
 const Checkbox = styled.input.attrs({ type: 'checkbox' })`
-  position: absolute;
-  width: 58px;
-  height: 58px;
-  top: 501px;
-  left: 691px;
-  border: 2px solid #000000;
+
+    width: 58px;
+    height: 58px;
+    
+    border: 2px solid #000000;
+
+    @media (max-width:1100px) {
+        width: 29px;
+        height: 29px;
+       
+    }
   
 `;
 
+const ButtonContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top:35px;
+ 
+`;
+
 const Button = styled.button`
-    position: absolute;
+   
     width: 107px;
     height: 64px;
-    top: 594px;
-    left: 667px;
+   
     border-radius: 18px;
     font-family: Inter;
     font-size: 28px;
     font-weight: 400;
     line-height: 34px;
     letter-spacing: 0em;
-    text-align: center;
     border: none; 
     
     color: ${props => (props.checked ? '#000000' : '#ffffff')};
     background-color: ${props => (props.checked ? '#5980EF': '#D9D9D9')};
     
+    @media (max-width: 1100px) {
+        width: 53px;
+        height: 32px;
+        font-size: 14px;
+        line-height: 22px;
+      }
 
 `;
 
 const Line = styled.hr`
-  position: absolute;
-  width: 1123.0111083984375px;
-  top: 865px;
-  left: 141px;
-  border: 1px solid black;
-  transform: rotate(0deg);
-  border: 1px solid #9FA0A3;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 1123.0111083984375px;
+    margin-top:250px;
+    
+    transform: rotate(0deg);
+    border: 1px solid #9FA0A3;
+
+    @media (max-width: 1100px) {
+        width: 90%;
+        margin-top:150px;
+    }
 
 `;
 
 const Download = styled.h1`
-    position: absolute;
-    width: 662px;
-    height: 34px;
-    top: 915px;
-    left: 372px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+   
+    margin-top: 30px;
+   
     font-family: Inter;
     font-size: 28px;
     font-weight: 400;
     line-height: 34px;
     letter-spacing: 0em;
-    text-align: left;
     color:black;
+
+    @media (max-width: 1100px) {
+        font-size: 14px;
+        line-height: 1px;
+      }
 
 
 `;
@@ -166,18 +216,21 @@ const Register=()=>{
             <RegisterText>회원가입</RegisterText>
             <RegisterText1>Trothly는 iOS 어플리케이션 Troth Cam에 가입해야 가입이 가능합니다.</RegisterText1>
             <RegisterText2>Troth Cam 회원가입이 되어있나요?</RegisterText2>
-            <Checkbox checked={checked} onChange={handleCheckboxChange} />
-            <Button checked={checked} onClick={handleButtonClick}>다음</Button>
+            <CheckboxContainer>
+                <Checkbox checked={checked} onChange={handleCheckboxChange} />
+            </CheckboxContainer>
+            <ButtonContainer>
+                <Button checked={checked} onClick={handleButtonClick}>다음</Button>
+            </ButtonContainer>
             
-            
-            
+
             <Line></Line>
             <Download>
                 <span>Troth Cam이 설치되어 있지 않습니까?</span>
-                <a href="https://your-download-url.com" target="_blank" rel="noopener noreferrer" style={{color:'#5980EF', textDecoration: 'none'}}> 지금 다운로드</a>
+                <a href="https://your-download-url.com" target="_blank" rel="noopener noreferrer" style={{color:'#5980EF', textDecoration: 'none'}}>&nbsp;지금 다운로드</a>
             </Download>
-              <FooterContainer>
-            <Footer />
+            <FooterContainer>
+                <Footer />
             </FooterContainer>
         </PageContainer>
     );
