65dd46cc3c2b64dfc27687d29c0f957e6c48c060
Shin Ihyeon <mashin2002@naver.com>
2023-08-06 02:49:50 +0900

    [MOD] 메인 제외

--- trothcam_web/src/components/Detail_me.jsx
+++ trothcam_web/src/components/Detail_me.jsx
@@ -5,84 +5,96 @@ import DetailBorard from './DetailBoard';
 import ShareIcon from "./img/share_icon.png";
 import CopyIcon from "./img/copy_icon.png";
 import MoreIcon from "./img/more_icon.png";
-
+import LockIcon from "./img/lock_icon.png";
+import ListIcon from "./img/list_icon.png";
 const UserToken = styled.div`
-  display: inline;
+    display: inline;
     color: #5980EF;
-    font-size: 70px;
-    weight: 700;
-    margin-left: 37px;
-    margin-top: 144px;
+    font-size: 45px;
+    margin-left: 21.8%;
+    margin-top: 87px;
+    font-weight: 600;
+    font-family: inter;
 `;
 const TabBtn = styled.button`
     color: #000000;
-    font-size: 30px;
+    font-size: 25px;
     font-family: inter;
     background-color: white;
     border: white;
     border-bottom: solid 3px ${props => (props.clicked ? '#5980EF' : '#ffffff')};
     padding-bottom: 5px;
     margin-left: 36px;
-
     font-size: #000000;
-    font-family: 400;
-`;
-const ButtonContainer = styled.div`
-display: flex;
-justify-content: space-between;
-margin-top:32px;
+    font-weight: 400;
 `;
 const LogoutBtn = styled.button`
-width: 140px;
-height: 50px;
+width: 104px;
+height: 37px;
 border-radius: 8px;
 border: 1px solid #FF0000;
-font-size: 20px;
+font-size: 15px;
 font-weight: 600;
 font-family: inter;
 background-color: #FF0000;
 color: white;
 `;
 const SettingsBtn = styled.button`
-width: 140px;
-height: 50px;
+width: 104px;
+height: 37px;
 border-radius: 8px;
 border: 1px solid black;
-font-size: 20px;
+font-size: 15px;
 font-weight: 600;
 font-family: inter;
 background-color: white;
-margin-right: 20px;
+margin-right: 10px;
 
 `;
 const ButtonDiv = styled.div`
-align-items: center;
-justify-content: center;
-margin-right: 50px;
-
+float: right;
+margin-right: 21%;
+margin-top: 15px;
 `;
 
 const RecentArrange = styled.div`
 font-family: inter;
-font-size: 25px;
+font-size: 20px;
 font-weight: 400;
 color: #000000;
-margin-left: 36px;
+margin-left: 21.8%;
 margin-top: 30px;
 `;
 const BtnDiv = styled.div`
   display: inline-block;
   float: right;
-  margin-right: 20px;
+  margin-right: 21.8%;
+
 `;
 const Btn = styled.button`
   border: solid 1px #9FA0A3;
-  width: 59px;
-  height: 59px;
+  width: 44px;
+  height: 45px;
   background-color: #FFFFFF00;
 `;
 const BtnImg = styled.img`
-
+width: 26px;
+height: 23px;
+`;
+const TabDiv = styled.div`
+  
+  margin-top: 70px;
+  margin-left: 21%;
+`;
+const ListImg = styled.img`
+  margin-right: 10px;
+`;
+const PhotoBoardContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(1, 1fr); // Four items in a row
+  gap: 20px; // Gap between items
+  margin: 0 auto; // Center the grid
+  max-width: 850px; // Maximum width for the grid (adjust as needed)
 `;
 const Detail_me = () =>{
     
@@ -92,6 +104,10 @@ const Detail_me = () =>{
     const photoList = [
         {"Name": "이름", "price": "17000", "owner": "시니"},
         {"Name": "이름2", "price": "17000", "owner": "시니현"},
+        {"Name": "이름", "price": "17000", "owner": "시니현"},
+        {"Name": "이름", "price": "17000", "owner": "시니현"},
+        {"Name": "이름", "price": "17000", "owner": "시니현"},
+        {"Name": "이름", "price": "17000", "owner": "시니현"},
         {"Name": "이름", "price": "17000", "owner": "시니현"},
       ];
 
@@ -120,21 +136,28 @@ const Detail_me = () =>{
           <Btn><BtnImg src={CopyIcon}/></Btn>
           <Btn style={{ borderBottomRightRadius: "5px", borderTopRightRadius:"5px"}}><BtnImg src={MoreIcon}/></Btn>
         </BtnDiv>
-        <ButtonContainer>
-            <div>
-                <TabBtn clicked={isTabButton1Clicked} onClick={clickTabBtn1}>보유 중인 인증서</TabBtn>
-                <TabBtn clicked={isTabButton2Clicked} onClick={clickTabBtn2}>상세 정보</TabBtn>
-                <TabBtn clicked={isTabButton3Clicked} onClick={clickTabBtn3}>관심 인증서</TabBtn>
-            </div>
-            <ButtonDiv>
+        <ButtonDiv>
                 <SettingsBtn>Settings</SettingsBtn>
                 <LogoutBtn>Log out</LogoutBtn>
             </ButtonDiv>
-        </ButtonContainer>
-         {(isTabButton1Clicked || isTabButton3Clicked) && <RecentArrange>최근 본 상품</RecentArrange>}
-        {isTabButton1Clicked && <PhotoBoard photoList={photoList} />}
+          <TabDiv>
+                <TabBtn clicked={isTabButton1Clicked} onClick={clickTabBtn1} style={{marginLeft:"0px"}}>보유 중인 인증서</TabBtn>
+                <TabBtn clicked={isTabButton2Clicked} onClick={clickTabBtn2}>상세 정보</TabBtn>
+                <TabBtn clicked={isTabButton3Clicked} onClick={clickTabBtn3}>비공개 인증서<BtnImg style={{marginLeft: "10px"}} src={LockIcon}/></TabBtn>
+            </TabDiv>
+        
+         {(isTabButton1Clicked || isTabButton3Clicked) && <RecentArrange><ListImg src={ListIcon}/>최근 보유 순</RecentArrange>}
+         {isTabButton1Clicked && (
+                <PhotoBoardContainer>
+                    <PhotoBoard photoList={photoList} />
+                </PhotoBoardContainer>
+            )}
         {isTabButton2Clicked && <DetailBorard />}
-        {isTabButton3Clicked && <PhotoBoard photoList={photoList} />}
+        {isTabButton3Clicked && (
+                <PhotoBoardContainer>
+                    <PhotoBoard photoList={photoList} />
+                </PhotoBoardContainer>
+            )}
         </>
     )
 };
--- trothcam_web/src/components/PhotoBoard.jsx
+++ trothcam_web/src/components/PhotoBoard.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import styled from "styled-components";
 import PhotoItem from './PhotoItem';
 
+const PhotoBoardDiv = styled.div`
+  
+`;
 const PhotoBoard = (props) => {
   return (
-    <>
+    <PhotoBoardDiv>
           {props.photoList.map((item, index) => (
           <PhotoItem index={index} item={item} />
         ))}
-    </>
+    </PhotoBoardDiv>
   );
 }
 
