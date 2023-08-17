3e558f698aa819e55d8b1342a31882a5d494128e
2hy2on <mashin0128@gmail.com>
2023-08-17 16:33:12 +0900

    product Detail 경로

--- trothcam_web/src/components/Login.jsx
+++ trothcam_web/src/components/Login.jsx
@@ -13,7 +13,7 @@ const Login = () => {
   const navigate = useNavigate();
 
   const handleLogin = () => {
-    const url = "/auth/login";
+    const url = "https://trothly.com/auth/login";
     const requestBody = {
       id,
       password,
